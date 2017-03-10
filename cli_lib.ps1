7fc571efc68fc290d922da0c2f57c086bc134644
Brendan Sapience <Brendan.sapience@gmail.com>
2017-03-10 18:15:26 -0500

    added checks on keywords in output

--- cli_lib.ps1
+++ cli_lib.ps1
@@ -3,6 +3,24 @@ $JAVACMD = "java -jar"
 $AUTHPARAMS = "-C 100 -L ARA -D ARA -W ARA -H AETestHost"
 $BINREPO = "C:\Users\brend\Documents\git\work\UC4-Automic-AE-CLI-Binary-Repository"
 
+function AddToATestCaseSuiteFullWithKeywordChecks
+{
+ $Binary = $args[0]
+ $Cmd = $args[1]
+ $Ret = $args[2]
+ $Keywords = $args[3]
+ $NegKeywords = $args[4]
+ $myObject = New-Object System.Object
+ $myObject | Add-Member -type NoteProperty -name BinaryName -Value $Binary
+ $myObject | Add-Member -type NoteProperty -name Command -Value $Cmd
+ $myObject | Add-Member -type NoteProperty -name RetCodeExpected -Value $Ret
+ $myObject | Add-Member -type NoteProperty -name KeywordsExpected -Value $Keywords
+ $myObject | Add-Member -type NoteProperty -name KeywordsExcluded -Value $NegKeywords
+
+  $global:TestCaseArray += $myObject
+ #return $myObject
+}
+
 function AddToATestCaseSuiteFull
 {
  $Binary = $args[0]
@@ -37,4 +55,38 @@ function CheckReturnCode
    return $false
  }
 
+}
+
+function CheckOutputForKeywords
+{
+ $Keywords = $args[0]
+ $Output = $args[1]
+
+ #write-host $Output
+ $GLOBAL = $true
+ $Keywords.Split(",") | ForEach {
+ #write-host "Testing token: $_"
+ $FOUND =Select-String -inputObject $Output -Quiet "$_" 
+    if(-Not($FOUND)){
+        $GLOBAL = $false
+       }
+ }
+ return $GLOBAL
+}
+
+function CheckOutputForKeywordsReverse
+{
+ $Keywords = $args[0]
+ $Output = $args[1]
+
+ #write-host $Output
+ $GLOBAL = $true
+ $Keywords.Split(",") | ForEach {
+ #write-host "Testing token: $_"
+ $FOUND =Select-String -inputObject $Output -Quiet "$_" 
+    if($FOUND){
+        $GLOBAL = $false
+       }
+ }
+ return $GLOBAL
 }
--- tests_JOBSUpdate.ps1
+++ tests_JOBSUpdate.ps1
@@ -6,27 +6,25 @@ cd $BINREPO
 
 $JavaBin = "JOBS_Update.jar"
 
-
- 
- AddToATestCaseSuiteFull $JavaBin '-commit -name "JAVA.API.JOBS.JOBS_WIN.1" -u_active N' 0
- AddToATestCaseSuiteFull $JavaBin '-commit -name "JAVA.API.JOBS.JOBS_WIN.1" -u_genatruntime Y' 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_addvar `"['Allo#','My Value']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_title `"['.*','My New Title']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_login `"['.*','LOGIN.GENERAL']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_host `"['.*','WIN01']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_arch1 `"['.*','My New Archive 1']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_arch2 `"['.*','My New Archive 2']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deamode `"EFEXEC`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deastatus `"ENDED_OK`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deadelay 20" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_maxruns 20" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_process `"['.*','My New Process!']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_preprocess `"['.*','My New Pre Process!']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_postprocess `"['.*','My New Post Process!']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_priority 50" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_queue `"['.+','MY.QUEUE']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_updvar `"['Allo#','My New Value']`"" 0
- AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_updvar `"['NOVAR#','My New Value']`"" 0
+ AddToATestCaseSuiteFullWithKeywordChecks $JavaBin '-commit -name "JAVA.API.JOBS.JOBS_WIN.1" -u_active N' 0 "Job Set To Inactive" "Error"
+ AddToATestCaseSuiteFullWithKeywordChecks $JavaBin '-commit -name "JAVA.API.JOBS.JOBS_WIN.1" -u_genatruntime Y' 0 "Generate At Runtime Activated" "Error"
+ AddToATestCaseSuiteFullWithKeywordChecks $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_addvar `"['Allo#','My Value']`"" 0 "Add Variable - Name: Allo# | Value: My Valu" "Error"
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_title `"['.*','My New Title']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_login `"['.*','LOGIN.GENERAL']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_host `"['.*','WIN01']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_arch1 `"['.*','My New Archive 1']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_arch2 `"['.*','My New Archive 2']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deamode `"EFEXEC`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deastatus `"ENDED_OK`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_deadelay 20" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_maxruns 20" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_process `"['.*','My New Process!']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_preprocess `"['.*','My New Pre Process!']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_postprocess `"['.*','My New Post Process!']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_priority 50" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_queue `"['.+','MY.QUEUE']`"" 0
+ #AddToATestCaseSuiteFull $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_updvar `"['Allo#','My New Value']`"" 0
+ AddToATestCaseSuiteFullWithKeywordChecks $JavaBin "-commit -name 'JAVA.API.JOBS.JOBS_WIN.1' -u_updvar `"['NOVAR#','My New Value']`"" 0 "Could Not Update,Variable Not Found" "Error"
 
 Write-Host "Number of Test Cases Found: $($TestCaseArray.Count)"
 
@@ -39,15 +37,39 @@ foreach ($testcase in $TestCaseArray) {
   Write-Host " %% Running Test: $command"
   $commandOutput = iex "& $command"
   $commandReturnCode = $lastExitCode
-
+  #echo $commandOutput
   $Status = CheckReturnCode $($testcase.RetCodeExpected) $commandReturnCode
   if(-Not($Status)){
   Write-Host " -- Test Failed: $($testcase.Command). Expected: $($testcase.RetCodeExpected) Found: $commandReturnCode "
-  write-Host " Output is: "
-  echo $commandOutput
+  #write-Host " Output is: "
+ 
   }else{
-  Write-Host " ++ Test OK: $($testcase.Command)."
-  #echo $commandOutput
+
+      $ErrorFound = $false
+      if($($testcase.KeywordsExpected)){
+        
+        $LogCheck = CheckOutputForKeywords $($testcase.KeywordsExpected) $commandOutput
+        if(-Not($LogCheck)){
+            Write-Host " -- Test Failed on Log Check. Not Found: [$($testcase.KeywordsExpected)]"
+            $ErrorFound = $true
+        }else{
+            #Write-Host " ++ Test OK: $($testcase.Command)."
+        }
+       }
+      if($($testcase.KeywordsExcluded)){
+        $LogCheck = CheckOutputForKeywordsReverse $($testcase.KeywordsExcluded) $commandOutput
+        if(-Not($LogCheck)){
+            Write-Host " -- Test Failed on Log Check. Found: [$($testcase.KeywordsExcluded)]"
+            $ErrorFound = $true
+        }else{
+            #Write-Host " ++ Test OK: $($testcase.Command)."
+        }
+       }
+
+       if(-Not($ErrorFound)){
+        Write-Host " ++ Test OK: $($testcase.Command)."
+        #echo $commandOutput
+      }
   }
   
  }
